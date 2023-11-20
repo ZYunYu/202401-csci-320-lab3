6599ccb02bc526fb2a520d7164f890d5d989f5ff
Huixiang Zhang <z.yun.yu@outlook.com>
2023-11-20 00:00:40 -0500

    fix logic

--- lab3.c
+++ lab3.c
@@ -6,20 +6,40 @@
 extern int** sudoku_board;
 int* worker_validation;
 
-int** read_board_from_file(char* filename){
+int** read_board_from_file(char* filename) {
     FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("Error opening file");
+        return NULL;
+    }
 
     int** board = malloc(sizeof(int*) * 9);
-    for(int i = 0; i < 9; i++){
+    for (int i = 0; i < 9; i++) {
         board[i] = malloc(sizeof(int) * 9);
-        for (int j = 0; j < 9; j++){
-            board[i][j] = 0;
+    }
+
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            int read = fscanf(fp, j < 8 ? "%d," : "%d", &board[i][j]);
+            if (read != 1) {
+                perror("Error reading sudoku board");
+                // Add more detailed error printing here
+                printf("Failed to read number at row %d, column %d\n", i + 1, j + 1);
+                // Free allocated memory and return
+                for (int k = 0; k <= i; k++) {
+                    free(board[k]);
+                }
+                free(board);
+                fclose(fp);
+                return NULL;
+            }
         }
     }
-    
+
     fclose(fp);
     return board;
 }
+
 
 
 // check rows
@@ -38,7 +58,6 @@ void* check_row(void* arg) {
     return (void*)1;
 }
 
-// check columns
 void* check_col(void* arg) {
     param_struct* params = (param_struct*)arg;
     int col = params->starting_col;
@@ -54,7 +73,6 @@ void* check_col(void* arg) {
     return (void*)1;
 }
 
-// check cubes
 void* check_cubes(void* arg) {
     param_struct* params = (param_struct*)arg;
     int startRow = params->starting_row;
@@ -73,13 +91,13 @@ void* check_cubes(void* arg) {
     return (void*)1;
 }
 
-
-// is_board_valid 
-int is_board_valid(){
+int is_board_valid() {
     int num_of_threads = 27; 
     pthread_t* tid = malloc(sizeof(pthread_t) * num_of_threads);
     pthread_attr_t attr;
     pthread_attr_init(&attr);
+
+    worker_validation = malloc(sizeof(int) * num_of_threads);
 
     param_struct* params = malloc(sizeof(param_struct) * num_of_threads);
 
@@ -103,18 +121,21 @@ int is_board_valid(){
     for (int i = 0; i < num_of_threads; i++) {
         void* result;
         pthread_join(tid[i], &result);
-        if (result == (void*)0) {
+        worker_validation[i] = (int)result;
+        if (worker_validation[i] == 0) {
             valid = 0;
             break;
         }
     }
 
+    free(worker_validation);
     free(params);
     free(tid);
     pthread_attr_destroy(&attr);
 
     return valid;
 }
+
 
 
     
